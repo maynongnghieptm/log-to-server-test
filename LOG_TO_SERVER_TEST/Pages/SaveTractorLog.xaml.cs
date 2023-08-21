1c560f230ca28e40706d386f95ff18cd985c3789
sonnguyenhong <sonnguyenhong291@gmail.com>
2023-08-21 16:55:31 +0700

    change save log event and message

--- LOG_TO_SERVER_TEST/Pages/SaveTractorLog.xaml.cs
+++ LOG_TO_SERVER_TEST/Pages/SaveTractorLog.xaml.cs
@@ -19,6 +19,7 @@ namespace LOG_TO_SERVER_TEST.Pages
         MainWindow mainWindow;
         WebSocketService webSocketService;
         Thread sendLogsThread;
+        string tractorid = "64e2241bf3ea921e3f7855bb";
         bool sending = false;
         public SaveTractorLog(MainWindow mainWindow)
         {
@@ -34,14 +35,13 @@ namespace LOG_TO_SERVER_TEST.Pages
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string host = serverHostInput.Text;
-            string tractorid = "64e2241bf3ea921e3f7855bb";
             try
             {
                 webSocketService = new WebSocketService(new SocketIO(host, new SocketIOOptions
                 {
                     ExtraHeaders = new Dictionary<string, string>
                     {
-                        { "tractorid", tractorid }
+                        { "tractorid", this.tractorid }
                     }
                 }));
                 webSocketService.connectWSServer();
@@ -52,7 +52,7 @@ namespace LOG_TO_SERVER_TEST.Pages
                     {
                         statusConnectionTbx.Text = "Connect to WS Server successfully!";
                     });
-                    client.On(tractorid, async (message) =>
+                    client.On(this.tractorid, async (message) =>
                     {
                         Debug.WriteLine("Hello");
                         var dataArray = JArray.Parse(message.ToString());
@@ -89,7 +89,7 @@ namespace LOG_TO_SERVER_TEST.Pages
 
         private void sendLogs(WebSocketService webSocketService, string message)
         {
-            webSocketService.sendTractorLogs("logs", message);
+            webSocketService.sendTractorLogs($"{this.tractorid}-logs", message);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -104,21 +104,21 @@ namespace LOG_TO_SERVER_TEST.Pages
             {
                 sending = true;
                 int cnt = 0;
-                string msg = "{\"t\":\"0\",\"data\":{\"id\":[\"1\",\"1\"],\"drive\":[0,1,2,0,0],\"time\":[26,12,2022,9,32,45,97],\"llh\":[20.9553559616953,105.846090000165,0],\"xyz\":[48587961.6456527,2317439.06053528,0],\"ypr\":[0,0,0],\"rfbs\":[0,0,0,0],\"tha\":[0,0,0],\"bt\":[0,0],\"frs\":[0,24.48,0,0],}}";
+                string msg = "{\"ver\":\"2\",\"id\":[\"34d45345\",\"gt435345\",\"235643gret\"],\"drive\":[36,2,0,65,26],\"time\":[17,12,2023,12,45,979,7],\"sum\":[12323,5543,34545.67,344.234,10.5,4.5],\"llh\":[-20.23213123,123.12312323,122.33],\"xyz\":[123123.123123,15469452.342,122.33],\"ypr\":[123.2,-12.34,123.22],\"rtk\":[234,23,23,2],\"sen\":[23,34,0,0,0,0,0,0,0,0,0,0,0],\"ctr_fed\":[12,23,1020,1001,1,1.0,2,2.0,-1,-1.0,1.21,1.19,35,35.01,5.6,5.4,4.5,4.4],\"ctr_oly\":[1,0,0,0,1,90,90,90],\"plans\":[20.23424,106.123233,20.92342423,106.2123233,20.234224,106.1232633,20.234264,106.8123233,20.234524,106.1232233,20.24346724,106.123233,20.23424,106.1238233,20.9232424,106.1223233,20.235424,106.1232363,20.23424,106.123233,20.232424,106.123233,20.423424,106.1232233,20.23424,106.1232833,20.23424,106.1232233,20.234724,106.1823233],\"err\":[0,1,123,213,122,475,45747]}";
                 while (sending)
                 {
                     if (cnt < 1)
                     {
-                        sendLogs(webSocketService, this.mainWindow.GetTractor().GetId() + "|" + this.mainWindow.GetField().GetId() + "|1668589200000|" + msg + "|1");
+                        sendLogs(webSocketService, msg);
                         cnt++;
                     }
                     else if(cnt < 15)
                     {
-                        sendLogs(webSocketService, this.mainWindow.GetTractor().GetId() + "|" + this.mainWindow.GetField().GetId() + "|1668589200000|" + msg + "|2");
+                        sendLogs(webSocketService, msg);
                         cnt++;
                     } else
                     {
-                        sendLogs(webSocketService, this.mainWindow.GetTractor().GetId() + "|" + this.mainWindow.GetField().GetId() + "|1668589200000|" + msg + "|0");
+                        sendLogs(webSocketService, msg);
                         //sending = false;
                         cnt = 0;
                     }
